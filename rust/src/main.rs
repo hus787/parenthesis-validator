89c42af6dc9ce38f1d175cae40253f3fa0926f0d
Hussain Parsaiyan <hus787@gmail.com>
2024-09-16 11:50:43 +0330

    rust port. tests added

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,45 @@
 use std::collections::HashMap;
+use tracing::info;
 
 fn main() {
-    let open_close_pairs = HashMap::from([('(', ')'), ('{', '}'), ('[', ']')]);
+    tracing_subscriber::fmt::init();
+}
+fn are_parenthesis_valid(text: &str) -> bool {
+    _are_parenthesis_valid("", text.split("").collect());
+    return false;
 }
 
-fn are_Parenthesis_Valid(before: String, string: Vec<String>) -> bool {
+fn _are_parenthesis_valid(before: &str, string: Vec<&str>) -> bool {
+    let open_close_pairs = HashMap::from([('(', ')'), ('{', '}'), ('[', ']')]);
     return false;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test() {
+        tracing_subscriber::fmt::init();
+        let test_cases = HashMap::from([
+("(", false),
+("()", true),
+("([)", false),
+(")", false),
+("())", false),
+("([{}])()[]{}", true),
+("({[(())]})()", true),
+("({[(())]})())", false),
+("(((()", false),
+("(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((())((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((()", false),
+("sdfasdf", false),
+("(asdf)", false),
+    ]
+    );
+        for (testStr, expectation) in test_cases.into_iter() {
+            info!("request made");
+            assert_eq!(are_parenthesis_valid(testStr), expectation);
+        }
+    }
 }
